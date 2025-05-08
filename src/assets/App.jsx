42e300c390ff0af5bdb6cab0c8f2e796b2ca9343
Zamu-ai <uriel.b.zambrano@gmail.com>
2025-05-08 11:30:09 -0300

    ejercicio adicional: lista de productos [urielzambrano]

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -29,24 +29,5 @@ import './App.css';
     </>
  )
  }  
-  return (
-    <>
-      <div className='EscribirLista'>
-        <h1>Lista de Tareas</h1>
-        <div className='Cantidad de Tareas'> 
-          <h3>Numero de Tareas:4</h3>
-          <h3>Pendientes:3</h3>
-  
-        </div>
-        <div className='AgregadoTarea'>
-           <h3>Agregar Tarea</h3>
-           <AgregarTarea/>
-        </div>
-       <Listas/>
-      </div>
-  
-  
-        <Listas/>
-    </>
- )
+ 
 export default App;
